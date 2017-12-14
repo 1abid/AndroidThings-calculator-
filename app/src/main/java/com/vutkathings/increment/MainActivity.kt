23d8fdfde5808de89959b57dd7bf3376592673ac
Abid Hasan <la4508@gmail.com>
2017-12-14 14:44:17 +0600

    some ui binding

--- app/src/main/java/com/vutkathings/increment/MainActivity.kt
+++ app/src/main/java/com/vutkathings/increment/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.vutkathings.increment
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.Snackbar
@@ -10,10 +12,10 @@ import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.messages.MessageListener
-import com.vutkathings.increment.BR.result
 import com.vutkathings.increment.databinding.ActivityMainBinding
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() , GoogleApiClient.ConnectionCallbacks , GoogleApiClient.OnConnectionFailedListener {
 
@@ -39,24 +41,54 @@ class MainActivity : AppCompatActivity() , GoogleApiClient.ConnectionCallbacks ,
 
     private val TAG = MainActivity::class.java.simpleName
 
+    private val keyUUID: String = "UUID_KEY"
+
     private lateinit var calculation : Calculate
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this , R.layout.activity_main)
-        //binding.calculate = calculation
+        calculation = savedInstanceState?.get(savedInstance) as Calculate? ?:
+                        Calculate.builder(getUUID(getSharedPreferences(applicationContext.packageName , Context.MODE_PRIVATE)) ,
+                                "0" , "0" , Operator.OPERATOR_PLUS)
 
-        //calculation = savedInstanceState?.get(savedInstance) as Calculate
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this , R.layout.activity_main)
+        binding.calculate = calculation
+
+        setOperatorDrawable(calculation.operator)
 
         buildGoogleApiClient()
 
     }
 
+    private fun setOperatorDrawable(operator: String?) {
+
+        when(operator){
+            "Plus" -> operatorIV.setImageResource(android.R.drawable.ic_input_add)
+            "Minus" -> operatorIV.setImageResource(R.drawable.ic_minus_symbol)
+            "Multiply" -> operatorIV.setImageResource(R.drawable.ic_multiply)
+            else -> operatorIV.setImageResource(R.drawable.ic_divide)
+        }
+    }
+
+
+    private fun getUUID(sharedPreferences: SharedPreferences): String {
+        var uuid = sharedPreferences.getString(keyUUID , "")
+
+        if(uuid.isEmpty()){
+            uuid.apply {
+                UUID.randomUUID().toString()
+                sharedPreferences.edit().putString(keyUUID , this).apply()
+            }
+        }
+
+        return uuid
+    }
+
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
 
-        //outState?.putSerializable(savedInstance , calculation)
+        outState?.putSerializable(savedInstance , calculation)
     }
 
     private fun buildGoogleApiClient() {
