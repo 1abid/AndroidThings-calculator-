5c2d5f1c315566404430afba138d1a4b1a79ae0f
Abid Hasan <la4508@gmail.com>
2017-12-14 14:06:10 +0600

    google api connection

--- app/src/main/java/com/vutkathings/increment/MainActivity.kt
+++ app/src/main/java/com/vutkathings/increment/MainActivity.kt
@@ -2,16 +2,22 @@ package com.vutkathings.increment
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.ArrayAdapter
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.messages.MessageListener
+import com.vutkathings.increment.BR.result
 import com.vutkathings.increment.databinding.ActivityMainBinding
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() , GoogleApiClient.ConnectionCallbacks , GoogleApiClient.OnConnectionFailedListener {
+
+
 
     /**
      * The entry point to Google Play Services.
@@ -31,15 +37,17 @@ class MainActivity : AppCompatActivity() , GoogleApiClient.ConnectionCallbacks ,
 
     private val savedInstance = Calculate::class.java.simpleName
 
+    private val TAG = MainActivity::class.java.simpleName
+
     private lateinit var calculation : Calculate
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this , R.layout.activity_main)
-        binding.calculate = calculation
+        //binding.calculate = calculation
 
-        calculation = savedInstanceState?.get(savedInstance) as Calculate
+        //calculation = savedInstanceState?.get(savedInstance) as Calculate
 
         buildGoogleApiClient()
 
@@ -48,25 +56,36 @@ class MainActivity : AppCompatActivity() , GoogleApiClient.ConnectionCallbacks ,
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
 
-        outState?.putSerializable(savedInstance , calculation)
+        //outState?.putSerializable(savedInstance , calculation)
     }
 
     private fun buildGoogleApiClient() {
 
+        if (mGoogleApiClient != null)
+            return
+
+        mGoogleApiClient.apply {
+            GoogleApiClient.Builder(this@MainActivity)
+                    .addApi(Nearby.MESSAGES_API)
+                    .addConnectionCallbacks(this@MainActivity)
+                    .enableAutoManage(this@MainActivity,this@MainActivity)
+                    .build()
+        }
     }
 
 
 
     override fun onConnected(bundle: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.i(TAG, "GoogleApiClient connected")
+        Snackbar.make(resultTv , "GoogleApiClient connected" , Snackbar.LENGTH_SHORT).show()
     }
 
     override fun onConnectionSuspended(i: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Snackbar.make(resultTv , "Google Api connection Suspended" , Snackbar.LENGTH_SHORT).show()
     }
 
     override fun onConnectionFailed(connectionResult: ConnectionResult) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Snackbar.make(resultTv , "connection Failed" +connectionResult.errorMessage , Snackbar.LENGTH_SHORT).show()
     }
 
 
